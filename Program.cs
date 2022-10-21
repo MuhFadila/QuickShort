7e6260e3752bfd7c8d1eece84a5f98679a4eae1f
Muhammad Fadila <muh.fadila18@gmail.com>
2022-10-21 16:51:41 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuickShort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of data comparasion
+        private int mov_count = 0; // number of data movements
+
+        // number of elements in array
+        private int n;
+
+        void input()
+    }
+}
