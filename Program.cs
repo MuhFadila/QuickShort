abfc2db73fca3e750243a4667336069583f482f9
Muhammad Fadila <muh.fadila18@gmail.com>
2022-10-21 17:21:28 +0700

    menambahkan fungsi int temp

--- Program.cs
+++ Program.cs
@@ -39,6 +39,12 @@ namespace QuickShort
         // swaps the element at index y
 
         void swap(int x, int y)
-        {}
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
     }
 }
